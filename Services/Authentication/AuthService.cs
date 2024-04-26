b7f1044a13faddbdf7e4f39f1e92c86584b3bc43
Fletcher Trueblood <ftrueblood@jgboswell.com>
2024-04-26 16:04:39 -0700

    Clean up
    Remove unused usings

--- Services/Authentication/AuthService.cs
+++ Services/Authentication/AuthService.cs
@@ -2,12 +2,7 @@
 using Domain.Interfaces.Services.Application;
 using Domain.Interfaces.Services.Authorization;
 using Domain.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Services.Authentication
 {
